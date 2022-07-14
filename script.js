1e24c5324f42abec5bca27f6683715d60a3bff06
Sidra Naeem <107788062+sidranaeemsidi@users.noreply.github.com>
2022-07-14 07:35:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#poper').popover({
+        html: true,
+        content: $('#pop')
+    });
+
+});
+
+$(document).ready(function(){
+    $('#box').click(function(){
+        $('#box').css('background', 'red');
+
+    });
+
+});
